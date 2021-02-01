b352cb3b478e86ca5202703a948705f81cac45cd
Chelsea Becker <63199870+cschweig2@users.noreply.github.com>
2021-02-01 14:26:28 -0800

    add to.Fixed to function
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- src/coinCounter.js
+++ src/coinCounter.js
@@ -12,13 +12,16 @@ export default function coinCounter(x, quarters = 0, dimes = 0, nickels = 0, pen
       dimes = x / .10;
       dimes = Math.floor(dimes);
       x -= (dimes * .10);
+      x = x.toFixed(2);
     } else if (x < .10 && x >= .05) {
       nickels = x / .05;
       nickels = Math.floor(nickels);
       x -= .05;
+      x = x.toFixed(2);
     } else {
       pennies = x / .01;
       x = 0;
+      x = x.toFixed(2);
     }
     return coinCounter(x, quarters, dimes, nickels, pennies);
   }
